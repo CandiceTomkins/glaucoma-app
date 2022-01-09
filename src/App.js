b720c1c3f2a77ff11e3a31b5172619e5275cff8e
CandiceTomkins <87971865+CandiceTomkins@users.noreply.github.com>
2022-01-09 13:55:56 +0000

    Init commit. Installed GoogleFonts and Bootstrap. Added input.

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from "./eye-purple-blue.jpeg";
+import Dictionary from "./Dictionary";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <main>
+        <h1>Glaucoma App</h1>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>coded by Candice Tomkins</small>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Dictionary.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Dictionary.css";
+
+export default function () {
+  return (
+    <div className="container">
+      <div className="Dictionary">
+        <form>
+          <input
+            className="height: 100px"
+            type="search"
+            placeholder="Search for something..."
+          />
+
+          <input
+            className="Dictionary-search-button"
+            type="submit"
+            value="👀 Click Here"
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
