2dc8617df0721767abb381e0aa01e6707e93055b
Candice Tomkins <candicetomkins@gmail.com>
2022-01-15 11:44:28 +0000

    added my Story title and image

--- src/MyStory.js
+++ src/MyStory.js
@@ -1,13 +1,30 @@
 import React from "react";
 import "./MyStory.css";
+import candy from "candy-yellow-white-house.jpeg";
 
 export default function MyStory() {
   return (
-    <div className="myStory">
-      <p>
-        My name is Candy. I'm 33 and I was diagnosed with chronic angle-closure
-        glaucoma in 2021.
-      </p>
+    <div className="container myStory">
+      <div className="row">
+        <div className="col-6">
+          <h2>My Story</h2>
+        </div>
+        <div className="col-6">
+          <img
+            src={candy}
+            alt="candy-yellow-white-house"
+            className="candy img-fluid"
+          />
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <p>
+              My name is Candy. I'm 33 and I was diagnosed with chronic
+              angle-closure glaucoma in 2021.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
