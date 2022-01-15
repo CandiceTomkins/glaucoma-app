36ad914a3257c9d33f0c77051c6907788396f396
Candice Tomkins <candicetomkins@gmail.com>
2022-01-15 16:25:56 +0000

    removed dead logo files

--- src/Results.js
+++ src/Results.js
@@ -4,7 +4,7 @@ export default function Result(props) {
   if (props.results) {
     return (
       <div className="results">
-        <h2>{props.results.word}</h2>
+        <h2>hello</h2>
       </div>
     );
   } else {
