aa9754610ca1561cc3603a2de68fb3a582f623b4
Candice Tomkins <candicetomkins@gmail.com>
2022-01-15 17:57:08 +0000

    dictionary css

--- src/Results.js
+++ src/Results.js
@@ -7,6 +7,7 @@ export default function Results(props) {
       <div className="Results">
         <h2>{props.results.meta.id}</h2>
         <p>{props.results.shortdef}</p>
+        <p>{props.results.hom.hwi}</p>
       </div>
     );
   } else {
