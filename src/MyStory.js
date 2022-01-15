b1ab874f5da5828909c2a6dc6096b66210409ca3
Candice Tomkins <candicetomkins@gmail.com>
2022-01-15 11:52:37 +0000

    removed grid

--- src/MyStory.js
+++ src/MyStory.js
@@ -5,26 +5,18 @@ import candy from "./candy-yellow-white-house.jpeg";
 export default function MyStory() {
   return (
     <div className="container myStory">
-      <div className="row">
-        <div className="col-6">
-          <h2>My Story</h2>
-        </div>
-        <div className="col-6">
-          <p>
-            My name is Candy. I'm 33 and I was diagnosed with chronic
-            angle-closure glaucoma in 2021.
-          </p>
-        </div>
-        <div className="row">
-          <div className="col-6">
-            <img
-              src={candy}
-              alt="candy-yellow-white-house"
-              className="candy img-fluid"
-            />
-          </div>
-        </div>
-      </div>
+      <h2>My Story</h2>
+
+      <p>
+        My name is Candy. I'm 33 and I was diagnosed with chronic angle-closure
+        glaucoma in 2021.
+      </p>
+
+      <img
+        src={candy}
+        alt="candy-yellow-white-house"
+        className="candy img-fluid"
+      />
     </div>
   );
 }
