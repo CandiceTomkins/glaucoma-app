0ccd2ace2c6a5e3fab1cbb718bb3dc6124d9b9c8
CandiceTomkins <candicetomkins@gmail.com>
2022-02-17 18:33:30 +0000

    meds update

--- src/Medication.js
+++ src/Medication.js
@@ -1,12 +1,26 @@
 import React from "react";
+import "./Medication.css";
 
 export default function Medication() {
   return (
-    <div className="container medication">
+    <div className="container medication mb-5">
       <h2 className="section-title">Meds</h2>
-      <h3>Timolol</h3> <h3>Dexamethasone</h3> <h3>Bimatoprost</h3>{" "}
-      <h3>Dorzolamide</h3>
-      <h3>Acetazolamide</h3> <h3>Brimonidine</h3>
+      <h3 className="drug-name">Timolol</h3>
+      <p className="medication-type mb-5">
+        <strong>Type:</strong> Beta-Blocker
+        <br />
+        <strong>How it works:</strong> Decreases the amount of fluid within the
+        eye
+        <br />
+        <strong>Brand Names:</strong> Betimol.Istalol.Timoptic.
+        Combigan(combined with brimonidine)
+        <br />
+      </p>
+      <h3 className="drug-name">Dexamethasone</h3>{" "}
+      <h3 className="drug-name">Bimatoprost</h3>{" "}
+      <h3 className="drug-name">Dorzolamide</h3>
+      <h3 className="drug-name">Acetazolamide</h3>{" "}
+      <h3 className="drug-name">Brimonidine</h3>
     </div>
   );
 }
